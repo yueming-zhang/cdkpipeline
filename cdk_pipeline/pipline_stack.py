260b4a445b7f50fc485186e0f928684a5d1c9b2a
ming.y.zhang@gmail.com <ming.y.zhang@gmail.com>
2021-06-20 13:54:47 -0700

    add pre-prod stage

--- cdk_pipeline/pipline_stack.py
+++ cdk_pipeline/pipline_stack.py
@@ -3,6 +3,8 @@ from aws_cdk import aws_codepipeline as codepipeline
 from aws_cdk import aws_codepipeline_actions as cpactions
 from aws_cdk import pipelines
 
+from .webservice_stage import WebServiceStage
+
 class PiplineStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
@@ -10,7 +12,7 @@ class PiplineStack(cdk.Stack):
         source_artifact = codepipeline.Artifact()
         cloud_assembly_artifact = codepipeline.Artifact()
 
-        pipelines.CdkPipeline(self, 'Pipeline',
+        pipeline = pipelines.CdkPipeline(self, 'Pipeline',
             cloud_assembly_artifact = cloud_assembly_artifact,
             pipeline_name='CDKPipeline',
 
@@ -29,3 +31,10 @@ class PiplineStack(cdk.Stack):
                 synth_command='cdk synth')
 
             )
+
+        pipeline.add_application_stage(
+            WebServiceStage(self, 'Pre-Prod',env={
+                'account': '804197954628',#'334146477851'
+                'region': 'us-east-1',
+            })
+        )
